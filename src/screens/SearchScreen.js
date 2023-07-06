7fb4a5731d25ffa6324be5e2c6d405f8b43664e1
KushalRB <kushal.rajbhandari@fusemachines.com>
2023-07-06 18:59:43 +0545

    Error Handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,38 +1,43 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet} from "react-native"
+import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
 
 const SearchScreen = () => {
-    const [term, setTerm] = useState('')
-    const [result, setResult] = useState([])
+  const [term, setTerm] = useState("");
+  const [result, setResult] = useState([]);
+  const [errorMessage, setError] = useState("");
 
-    const searchApi = async ()=>{
-        const response = await yelp.get('/search',{
-            params:{
-                term: {term},
-                location: 'san jose',
-            }
-        })
-        setResult(response.data.businesses)
-
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          term: { term },
+          location: "san jose",
+        },
+      });
+      setResult(response.data.businesses);
+    } catch (e) {
+      setError("Something went wrong");
     }
-    return(
-        <View style={styles.background}>
-            <SearchBar 
-                term = {term} 
-                onTermChanged ={(newTerm) => setTerm(newTerm)}
-                onEndEdit = {()=> searchApi()}
-            />
-            <Text>We have found {result.length} results</Text>
-        </View>
-    )
-}
+  };
+  return (
+    <View style={styles.background}>
+      <SearchBar
+        term={term}
+        onTermChanged={(newTerm) => setTerm(newTerm)}
+        onEndEdit={() => searchApi()}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {result.length} results</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    background:{
-        backgroundColor: '#FFFFFF'
-    }
-})
+  background: {
+    backgroundColor: "#FFFFFF",
+  },
+});
 
-export default SearchScreen
+export default SearchScreen;
