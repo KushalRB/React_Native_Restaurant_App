e35d3a7d6b30a4113d92621fee8f2996ea480a76
KushalRB <kushal.rajbhandari@fusemachines.com>
2023-07-12 17:09:24 +0545

    Flat list Rendering

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const ResultsList = ({ title, results}) => {
   return (
     <View>
       <Text style={styles.titleStyle}>{title}</Text>
-      <Text>Results : {results.length}</Text>
+      <FlatList 
+        horizontal={true}
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({item})=>{
+            return <Text> {item.name} |</Text>
+        }
+        }
+      />
     </View>
   );
 };
