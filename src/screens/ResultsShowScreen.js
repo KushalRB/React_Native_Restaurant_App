e1387f4dfd5d4f3c3f0d8ea7455dc7c525726949
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-07-18 19:12:31 +0545

    Fetched a single restaurant

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,16 +1,31 @@
-import React from "react";
-import { Text,View, Image, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, Image, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 
-const ResultsShowScreen = ({navigation}) => {
-    const id = navigation.getParam('id')
-    console.log(id)
-    return(
-        <View>
-            <Text>Results Show</Text>
-        </View>
-    )
-}
+const ResultsShowScreen = ({ navigation }) => {
+  const id = navigation.getParam("id");
+  
+  const [result, setResult] = useState(null);
 
-const styles = StyleSheet.create({})
+  const getResult = async (business_id) => {
+    console.log('id :',business_id)
+    const response = await yelp.get(`/${business_id}`);
+    setResult(response.data);
+  };
 
-export default ResultsShowScreen
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  console.log(result);
+
+  return (
+    <View>
+      <Text>Results Show</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
