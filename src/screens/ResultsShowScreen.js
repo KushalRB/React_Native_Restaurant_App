6e7dabf8b2691ce8aa633362a6f90ed5d693e682
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-07-20 15:32:45 +0545

    Upgrading the app to use React Navigation v6

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, Image, StyleSheet, FlatList } from "react-native";
+import { View, Image, StyleSheet, FlatList } from "react-native";
 import yelp from "../api/yelp";
-import { ScrollView } from "react-native-gesture-handler";
 
-const ResultsShowScreen = ({ navigation }) => {
-  const id = navigation.getParam("id");
+const ResultsShowScreen = ({ route }) => {
+  const id = route.params.id
 
   const [result, setResult] = useState(null);
 
   const getResult = async (business_id) => {
-    console.log("id :", business_id);
     const response = await yelp.get(`/${business_id}`);
     setResult(response.data);
   };
