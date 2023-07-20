d1c0e201f5fa656d0d41fac6671497f2a741d2be
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-07-20 14:01:59 +0545

    Showing list of images

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, Image, StyleSheet } from "react-native";
+import { Text, View, Image, StyleSheet, FlatList } from "react-native";
 import yelp from "../api/yelp";
+import { ScrollView } from "react-native-gesture-handler";
 
 const ResultsShowScreen = ({ navigation }) => {
   const id = navigation.getParam("id");
-  
+
   const [result, setResult] = useState(null);
 
   const getResult = async (business_id) => {
-    console.log('id :',business_id)
+    console.log("id :", business_id);
     const response = await yelp.get(`/${business_id}`);
     setResult(response.data);
   };
@@ -17,15 +18,29 @@ const ResultsShowScreen = ({ navigation }) => {
     getResult(id);
   }, []);
 
-  console.log(result);
+  if(result == null){
+    return null;
+  }
 
   return (
     <View>
-      <Text>Results Show</Text>
+        <FlatList
+          data={result.photos}
+          keyExtractor={(photo) => photo}
+          renderItem={({item}) => {
+
+             return <Image style={styles.imageStyle} source={{ uri: item }} />;
+          }}
+        />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageStyle: {
+    height: 240,
+    width: 420,
+  },
+});
 
 export default ResultsShowScreen;
